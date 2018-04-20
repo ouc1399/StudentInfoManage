050a4dc2706603d3413c5796ccae7feba74d3283
ouc1399 <wp1109056318@126.com>
2018-04-20 11:19:40 +0800

    控制台输出正序

--- src/main/java/StuInfoDeal.java
+++ src/main/java/StuInfoDeal.java
@@ -9,6 +9,64 @@ import java.util.List;
 
 public class StuInfoDeal {
 
+    /**
+     * 总共有四列，分别为：学号 姓名 年龄 成绩  用0  1 2 3 代替，可对学号 年龄 成绩进行排序
+     * @param list
+     * @param num
+     * @return
+     */
+
+
+
+
+
+    //控制台输出正序
+    public static List<String> consoleShow(List<String> list,int num){   							//list代表从csv文件中获取到的数据，i代表要排序的对象
+        List<String> copyList = new ArrayList<String>(); 												//用于返回排好序的list对象
+        List<Integer> tmpList = new ArrayList<Integer>();
+        if(list.size()>0){
+            copyList.add(list.get(0));																	//将列名赋值给resList
+            list.remove(0);
+        }
+        int [] arr = new int[list.size()];																//存放排序的数组
+        int j=0;
+        //获取须要排序的数组
+        Iterator<String> it = list.iterator();
+        while(it.hasNext()){
+            String res = it.next();																		//迭代获取一行数据
+            String[] result = res.split(",");
+            arr[j] = Integer.parseInt(result[num]);
+            tmpList.add(Integer.parseInt(result[num]));													//按照list中排序索引的顺序，依次将数据放入tmpList当中
+            j++;
+        }
+        bubbleSort(arr);																				//对数组进行排序
+        //对list进行排序
+        for(int k=0;k<arr.length;k++){
+            int tmpNum = arr[k];
+            System.out.println("tmpNum："+tmpNum);
+            Iterator<Integer> itTmp = tmpList.iterator();
+            while(itTmp.hasNext()){
+                int temp = itTmp.next();
+                if(tmpNum==temp){
+                    int index = tmpList.indexOf(temp);												//获取到目标数值在tmpList中的下标
+                    tmpList.set(index, -1);
+                    copyList.add(list.get(index));
+                    list.set(index, "");
+                }
+            }
+        }
+        Iterator<String> ite = copyList.iterator();
+        while (ite.hasNext()){
+            System.out.println(ite.next());
+        }
+        return copyList;
+    }
+
+
+
+
+
+
 
     //获取CSV文件数据
     public static List<String> printInConsole(File file) throws Exception{
@@ -68,8 +126,4 @@ public class StuInfoDeal {
         for(int k=0;k<arr.length;k++)
             System.out.println(arr[k]);
     }
-
-
-
-
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,24 @@
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
 
     int[] arr={12,3,2,8,4};
     File file = new File("D:"+File.separator+"File"+File.separator+"sutdentInfo.csv");
+
+
+
+
+
+
+
+
+
+
+
 
     @Test
     public void Sort() throws Exception {
@@ -20,5 +33,22 @@ public class test {
     @Test
     public void getCSVData() throws Exception {
         StuInfoDeal.printInConsole(file);
+    }
+
+    @Test
+    public void showInConsole() throws Exception {
+        List<String> conList = new ArrayList<String>();
+        conList.add("学号,姓名,年龄,成绩");
+        conList.add("4,jack1,16,85");
+        conList.add("2,jack2,14,75");
+        conList.add("7,jack3,15,77");
+        conList.add("5,jack5,18,88");
+        conList.add("1,jack4,17,76");
+        conList.add("3,jack7,17,67");
+        conList.add("9,jack9,13,67");
+        conList.add("6,jack6,21,95");
+        conList.add("8,jack8,16,82");
+        conList.add("10,jack10,12,91");
+        StuInfoDeal.consoleShow(conList,0);
     }
 }
